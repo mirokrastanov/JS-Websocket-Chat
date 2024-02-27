66b8f323aa89af8ac8085df1d91c853ac0944f6f
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-02-27 17:45:47 +0200

    Add an activity timer to prevent inf typing

--- server/public/app.js
+++ server/public/app.js
@@ -27,6 +27,13 @@ msgInput.addEventListener('keypress', () => {
     socket.emit('activity', socket.id.substring(0, 5));
 });
 
+let activityTimer;
 socket.on('activity', (name) => {
     activity.textContent = `${name} is typing...`;
+
+    // Clear after 1 second
+    clearTimeout(activityTimer);
+    activityTimer = setTimeout(() => {
+        activity.textContent = '';
+    }, 1000);
 });

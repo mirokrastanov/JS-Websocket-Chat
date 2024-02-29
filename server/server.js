782463bfd303dab7cb062afeefb7235ad7d10255
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-02-29 18:45:03 +0200

    Move from common js to ES6 imports

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
 
 app.get('/', (req, res) => {
     res.send('Server root.');
 });
 
-app.listen(5000, () => console.log(`Server is running on port 5000...`));
+app.get('/api/auth/signup', (req, res) => {
+    console.log('signup');
+});
+
+app.get('/api/auth/login', (req, res) => {
+    console.log('login');
+});
+
+app.get('/api/auth/logout', (req, res) => {
+    console.log('logout');
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));

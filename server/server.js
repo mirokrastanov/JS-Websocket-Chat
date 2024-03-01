362818620f4174f0f4ccdea7911db7dcbf814504
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-01 22:26:40 +0200

    Add req body parser middleware

--- server/server.js
+++ server/server.js
@@ -4,17 +4,16 @@ import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/mongoDB.js';
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-
-app.get('/', (req, res) => {
-    res.send('Server root.');
-});
+dotenv.config();
 
 app.use('/api/auth', authRoutes);
+app.use(express.json()); // parse JSON from incoming requests (req.body)
 
-
+// app.get('/', (req, res) => {
+//     res.send('Server root.');
+// });
 
 app.listen(PORT, () => {
     connectToMongoDB();

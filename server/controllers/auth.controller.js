d157d2d743ffea0bb5ac803194f1400911a715a9
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-01 23:52:50 +0200

    Generate access token and set cookie

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
 import { genAvatar } from "../utils/avatarUtils.js";
+import generateTokenAndSetCookie from "../utils/genToken.js";
 
 export const signup = async (req, res) => {
     try {
@@ -16,7 +17,6 @@ export const signup = async (req, res) => {
             return res.status(400).json({ error: "Username already exists" });
         }
 
-        // TODO: HASH Password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -28,14 +28,21 @@ export const signup = async (req, res) => {
             profilePic: genAvatar(fullName),
         });
 
-        await newUser.save();
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullname: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic,
-        });
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+
     } catch (error) {
         console.log("Error in signup controller", error.message);
         res.status(500).json({ error: "Internal Server Error" });

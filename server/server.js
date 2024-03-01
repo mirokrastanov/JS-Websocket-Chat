8b2f2acac0fbbe8546a62e97e1279f025bcab77b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-01 23:25:55 +0200

    Update server body parsers

--- server/server.js
+++ server/server.js
@@ -8,13 +8,21 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use('/api/auth', authRoutes);
+// Body parsers
+app.use(express.urlencoded({ extended: false })); // parse form fields
 app.use(express.json()); // parse JSON from incoming requests (req.body)
 
-// app.get('/', (req, res) => {
-//     res.send('Server root.');
-// });
+// TODO: Cookie parsers
 
+// TODO: Auth middleware
+
+// TODO: Routes
+app.use('/api/auth', authRoutes);
+
+// TODO: Error handler
+
+
+// Run Server
 app.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server is running on port ${PORT}...`);

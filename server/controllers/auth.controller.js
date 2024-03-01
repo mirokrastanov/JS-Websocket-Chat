df3a1e14b38bd83db69508afed8f509c714585ed
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-01 22:27:11 +0200

    Update signup function

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,10 +1,41 @@
+import User from "../models/user.model.js";
+import { genAvatar } from "../utils/avatarUtils.js";
+
 export const signup = async (req, res) => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body;
 
-        res.send('signupUser');
-    } catch (error) {
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords don't match" });
+        }
 
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" });
+        }
+
+        // TODO: HASH Password
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: genAvatar(fullName),
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullname: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 };
 

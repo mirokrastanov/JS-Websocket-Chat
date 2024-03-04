6f9d763dab55b6c9e18da657401e1c0d90170039
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-04 22:48:59 +0200

    Add a cookie parser and messages sub router

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from './db/mongoDB.js';
 
 const app = express();
@@ -13,11 +15,13 @@ app.use(express.urlencoded({ extended: false })); // parse form fields
 app.use(express.json()); // parse JSON from incoming requests (req.body)
 
 // TODO: Cookie parsers
+app.use(cookieParser());
 
 // TODO: Auth middleware
 
 // TODO: Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 // TODO: Error handler
 

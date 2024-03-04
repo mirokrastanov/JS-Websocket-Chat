2f47700f5936eed94f226703428c5713446a8747
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-04 23:00:34 +0200

    Create a get route for messages (conversation)

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { sendMessage } from '../controllers/message.controller.js';
+import { getMessages, sendMessage } from '../controllers/message.controller.js';
 import userRouteGuard from '../middlewares/userRouteGuard.js';
 
 const router = express.Router();
 
+router.get('/:id', userRouteGuard, getMessages);
 router.post('/send/:id', userRouteGuard, sendMessage);
 
 export default router;

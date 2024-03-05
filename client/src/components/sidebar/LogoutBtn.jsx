cf489c6a9b663a872e0d8e72293e0296255fbcde
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-05 16:10:25 +0200

    Design logout button and add loading indicator

--- client/src/components/sidebar/LogoutBtn.jsx
+++ client/src/components/sidebar/LogoutBtn.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BiLogOut } from "react-icons/bi";
 
 const LogoutBtn = () => {
-  return (
-    // TODO
-    <div>LogoutBtn</div>
-  )
+    const [loading, setLoading] = useState(false); // TODO move to a hook
+    const logout = () => { }; // TODO move to a hook
+
+    return (
+        <div className='mt-auto'>
+            {!loading ? (
+                <BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+            ) : (
+                <span className='loading loading-spinner'></span>
+            )}
+        </div>
+    )
 }
 
 export default LogoutBtn;

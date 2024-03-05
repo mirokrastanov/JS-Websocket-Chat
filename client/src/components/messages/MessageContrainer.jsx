67ed975b719afe22e73602aa8cbb94a28421eac6
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-05 16:09:59 +0200

    Conditionally render initial view

--- client/src/components/messages/MessageContrainer.jsx
+++ client/src/components/messages/MessageContrainer.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Messages from './Messages';
 import MessageInput from './MessageInput';
+import NoChatSelected from './NoChatSelected';
 
 const MessageContainer = () => {
+    const [chatSelected, setChatSelected] = useState(false);
+
     return (
         <div className='md:min-w-[450px] flex flex-col'>
-            <>
-                {/* Header */}
-                <div className='bg-slate-500 px-4 py-2 mb-2'>
-                    <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
-                </div>
+            {!chatSelected ? (
+                <NoChatSelected />
+            ) : (
+                <>
+                    {/* Header */}
+                    <div className='bg-slate-500 px-4 py-2 mb-2'>
+                        <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
+                    </div>
 
-                <Messages />
-                <MessageInput />
-            </>
+                    <Messages />
+                    <MessageInput />
+                </>
+            )}
+
         </div>
     )
 }

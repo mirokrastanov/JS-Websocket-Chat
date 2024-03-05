cd5da016fa1d41618dc6d8c83d44f8ef2a6494b0
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-05 15:01:24 +0200

    Update login design

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -5,8 +5,7 @@ const Login = () => {
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
                 <h1 className='text-3xl font-semibold text-center text-white'>
-                    <span className='text-cyan-300'>Chat </span>
-                    Login
+                    <span className='text-cyan-300'>Chat </span>Login
                 </h1>
 
                 <form>
@@ -21,14 +20,19 @@ const Login = () => {
                         <label className='label p-2' htmlFor='password'>
                             <span className='text-base text-white'>Password</span>
                         </label>
-                        <input name='password' type='password' placeholder='Enter Password...' className='w-full input input-bordered h-10' />
+                        <input
+                            name='password'
+                            type='password'
+                            placeholder='Enter Password...'
+                            className='w-full input input-bordered h-10'
+                        />
                     </div>
                     <a href='#' className='text-sm hover:underline hover:text-cyan-300 mt-1 inline-block p-2'>
                         {"Don't"} have an account?
                     </a>
 
                     <div>
-                        <button className='btn btn-block btn-sm mt-2 text-white'>Login</button>
+                        <button className='btn btn-block btn-sm mt-2 text-white border border-slate-700'>Login</button>
                     </div>
                 </form>
             </div>

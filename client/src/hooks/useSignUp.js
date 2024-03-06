0a8f62eb6eba957c22efab70a606708dff7cf4aa
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 16:18:10 +0200

    Create signup hook

---
+++ client/src/hooks/useSignUp.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+const useSignUp = () => {
+    const [loading, setLoading] = useState(false);
+
+    const signUp = async (values) => {
+        const success = handleInputErrors(values);
+        if (!success) return;
+
+        setLoading(true);
+        try {
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values),
+            });
+
+            const data = await res.json();
+
+            console.log(data);
+
+        } catch (error) {
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { loading, signUp };
+};
+
+export default useSignUp;
+
+function handleInputErrors({ fullName, username, password, confirmPassword, gender }) {
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+        toast.error('Please fill in all fields');
+        return false;
+    }
+
+    if (password.length < 6) {
+        toast.error('Password must be at least 6 characters');
+        return false;
+    }
+
+    if (password !== confirmPassword) {
+        toast.error('Passwords don\'t match');
+        return false;
+    }
+
+    return true;
+}

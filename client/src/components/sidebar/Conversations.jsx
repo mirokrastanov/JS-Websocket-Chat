c1fe994a9094d880cdebd8f8ba236709b99d34fb
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 18:49:05 +0200

    Render real conversations

--- client/src/components/sidebar/Conversations.jsx
+++ client/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Conversation from "./Conversation";
+import useGetConversations from '../../hooks/useGetConversations';
+import { getRandomEmoji } from '../../utils/emojis';
 
 const Conversations = () => {
+    const { loading, conversations } = useGetConversations();
+
+    useEffect(() => {
+        console.log(conversations);
+    }, []);
+
     return (
         <div className='py-2 flex flex-col overflow-auto'>
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {loading
+                ? <span className='loading loading-spinner mx-auto'></span>
+                : conversations.length > 0
+                    ? <> {conversations.map((conversation, i) => (
+                        <Conversation
+                            key={conversation._id}
+                            conversation={conversation}
+                            emoji={getRandomEmoji()}
+                            isLast={i === conversations.length - 1}
+                        />))} </> : null}
         </div>
     )
 }

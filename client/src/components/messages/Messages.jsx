1ba6fb959661841936d6903b64af33c7985e4d0f
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 21:08:13 +0200

    Render real messages dynamically

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Message from './Message';
+import useGetMessages from '../../hooks/useGetMessages';
+import MessageSkeleton from '../skeletons/MessageSkeleton';
+let counter = 0;
 
 const Messages = () => {
+    const { messages, loading } = useGetMessages();
+    const lastMessageRef = useRef();
+
+    useEffect(() => {
+        console.log(counter++);
+        if (lastMessageRef.current) {
+            lastMessageRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [messages, loading])
+
+
     return (
-        <div className='px-4 flex-1 overflow-auto'>
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
+        <div className='px-4 flex-1 overflow-auto pb-4'>
+            {loading && [...Array(3)].map((_, i) => <MessageSkeleton key={`skeleton-${i}`} />)}
+            {!loading && messages.length === 0 && (
+                <p className='text-center py-4'>Send a message to start the conversation</p>
+            )}
+            {!loading && messages.length > 0 && (
+                messages.map((message, i) => (
+                    <div key={message._id} ref={lastMessageRef}>
+                        <Message message={message} />
+                    </div>
+                ))
+            )}
         </div>
     )
 }

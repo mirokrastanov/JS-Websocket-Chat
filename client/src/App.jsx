55edd2a56c8f91b4921970c362f2d56c4a2c48a4
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 16:19:03 +0200

    Add react hot toast's placeholder element to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,19 @@ import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
 import Home from './pages/home/Home';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
     return (
         <>
             <div className='p-4 h-screen flex items-center justify-center'>
+                <Toaster />
                 <Routes>
                     <Route exact path='/' element={<Home />} />
                     <Route path='/home' element={<Navigate to='/' />} />
                     <Route path='index' element={<Navigate to='/' />} />
-                    
+
                     <Route path='/signup' element={<SignUp />} />
                     <Route path='/login' element={<Login />} />
                 </Routes>

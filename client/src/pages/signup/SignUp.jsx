b32f1b451d3d2312adc9117047fd103f9ab81e2c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 16:56:32 +0200

    Add conditional loading to signup button

--- client/src/pages/signup/SignUp.jsx
+++ client/src/pages/signup/SignUp.jsx
@@ -80,7 +80,10 @@ const SignUp = () => {
                     </Link>
 
                     <div>
-                        <button className='btn btn-block btn-sm mt-2 text-white border border-slate-700'>Sign Up</button>
+                        <button className='btn btn-block btn-sm mt-2 text-white border border-slate-700'
+                            disabled={loading}>
+                            {loading ? <span className='loading loading-spinner'></span> : 'Sign Up'}
+                        </button>
                     </div>
                 </form>
             </div>

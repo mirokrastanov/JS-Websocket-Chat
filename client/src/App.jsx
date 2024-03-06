a98570fa08665d62861c21ef32b9c51ce26697bc
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 15:16:37 +0200

    Create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,21 @@ import './App.css';
 import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
 import Home from './pages/home/Home';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
 
     return (
         <>
-            <div className="p-4 h-screen flex items-center justify-center">
-                <Home />
+            <div className='p-4 h-screen flex items-center justify-center'>
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route path='/home' element={<Navigate to='/' />} />
+                    <Route path='index' element={<Navigate to='/' />} />
+                    
+                    <Route path='/signup' element={<SignUp />} />
+                    <Route path='/login' element={<Login />} />
+                </Routes>
             </div>
         </>
     )

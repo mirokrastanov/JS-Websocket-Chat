ff37274cf54e031a1b4f924bb21d040e6cb8d85b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 19:22:44 +0200

    Render fetched data

--- client/src/components/messages/MessageContrainer.jsx
+++ client/src/components/messages/MessageContrainer.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Messages from './Messages';
 import MessageInput from './MessageInput';
 import NoChatSelected from './NoChatSelected';
+import useConversation from '../../zustand/useConversation';
 
 const MessageContainer = () => {
-    const [chatSelected, setChatSelected] = useState(false);
+    const { selectedConversation, setSelectedConversation } = useConversation();
 
     return (
         <div className='md:min-w-[450px] flex flex-col'>
-            {!chatSelected ? (
+            {!selectedConversation ? (
                 <NoChatSelected />
             ) : (
                 <>
                     {/* Header */}
                     <div className='bg-slate-500 px-4 py-2 mb-2'>
-                        <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
+                        <span className='text-gray-300 pr-0.5'>To: </span>
+                        <span className='text-gray-200 font-bold'>{selectedConversation.fullName}</span>
                     </div>
 
                     <Messages />

a80ffd3a5103a4327a290c0991b237ca4669cda0
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 17:21:36 +0200

    Add notes for later dev

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -9,6 +9,17 @@ export const useAuthContext = () => {
 export const AuthContextProvider = ({ children }) => {
     const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem('authUser')) || null);
 
+    // TODO
+    // move all auth hooks here
+    // create checkUserStatus() method
+    // invoke checkUserStatus() on load via useEffect[]
+    // move all methods into a contextData object and 
+    //    PROVIDE that instead of what's currently provided
+    // ADD conditional (loading based) rendering of the children
+    // hence - have a loading state stored in here!
+
+    // check if more is necessary when optimizing the code later on >>>
+
     return (
         <AuthContext.Provider value={{ authUser, setAuthUser }}>
             {children}

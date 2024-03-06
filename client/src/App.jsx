9486f659c917d24d6b12369a1d45560bb1f19c2e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-06 16:41:02 +0200

    Protect routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,22 @@ import SignUp from './pages/signup/SignUp';
 import Home from './pages/home/Home';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { useAuthContext } from './context/AuthContext';
 
 function App() {
+    const { authUser } = useAuthContext();
 
     return (
         <>
             <div className='p-4 h-screen flex items-center justify-center'>
                 <Toaster />
                 <Routes>
-                    <Route exact path='/' element={<Home />} />
+                    <Route exact path='/' element={authUser ? <Home /> : <Navigate to='/login' />} />
                     <Route path='/home' element={<Navigate to='/' />} />
-                    <Route path='index' element={<Navigate to='/' />} />
+                    <Route path='/index' element={<Navigate to='/' />} />
 
-                    <Route path='/signup' element={<SignUp />} />
-                    <Route path='/login' element={<Login />} />
+                    <Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+                    <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
                 </Routes>
             </div>
         </>

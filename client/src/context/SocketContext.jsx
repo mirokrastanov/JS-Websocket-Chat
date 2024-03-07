21622602ffefe6f2b5637f59578b5f3a77103368
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-07 19:32:06 +0200

    Add socket listener for server and close on unmount

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -1,18 +1,33 @@
 import React, { createContext, useEffect, useState } from 'react';
+import { useAuthContext } from './AuthContext';
+import io from 'socket.io-client';
 
 export const SocketContext = createContext();
 
 export const SocketContextProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
     const [onlineUsers, setOnlineUsers] = useState([]);
+    const { authUser } = useAuthContext();
+
 
     useEffect(() => {
+        if (authUser) {
+            const socket = io('http://localhost:5000'); // backend
+            setSocket(socket);
+
+            return () => socket.close();
+        } else {
+            if (socket) {
+                socket.close();
+                setSocket(null);
+            }
+        }
 
     }, []);
 
 
     return (
-        <SocketContext.Provider value={{}}>
+        <SocketContext.Provider value={{ socket, onlineUsers }}>
             {children}
         </SocketContext.Provider>
     );

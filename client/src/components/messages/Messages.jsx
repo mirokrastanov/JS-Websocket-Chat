053f90bbb8af303dfa69dd8d8d025f9aa0cef2e9
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-08 20:48:03 +0200

    Render last message without the user seeing scrolling - instantly

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
 import Message from './Message';
 import useGetMessages from '../../hooks/useGetMessages';
 import MessageSkeleton from '../skeletons/MessageSkeleton';
@@ -7,24 +7,35 @@ import useListenMessages from '../../hooks/useListenMessages';
 const Messages = () => {
     const { messages, loading } = useGetMessages();
     useListenMessages();
-    const lastMessageRef = useRef();
 
+    const msgCtrRef = useRef();
     useEffect(() => {
-        if (lastMessageRef.current) {
-            lastMessageRef.current.scrollIntoView({ behavior: 'smooth' });
+        // console.log("Container ref:", msgCtrRef.current);
+        // console.log("Scroll height:", msgCtrRef.current.scrollHeight);
+        // console.log("Client height:", msgCtrRef.current.clientHeight);
+        if (msgCtrRef.current) {
+            msgCtrRef.current.scrollTop = msgCtrRef.current.scrollHeight;
         }
-    }, [messages, loading])
+    }, [messages, loading]);
+
+    // const lastMessageRef = useRef();
+    // useEffect(() => {
+    //     if (lastMessageRef.current) {
+    //         lastMessageRef.current.scrollIntoView({ behavior: 'smooth' });
+    //     }
+    // }, [messages, loading])
 
 
     return (
-        <div className='px-4 flex-1 overflow-auto pb-4 z-10'>
+        <div className='px-4 flex-1 overflow-auto pb-4 z-10' ref={msgCtrRef} style={{ height: '100%', overflowY: 'scroll' }}>
             {loading && [...Array(3)].map((_, i) => <MessageSkeleton key={`skeleton-${i}`} />)}
             {!loading && messages.length === 0 && (
                 <p className='text-center py-4'>Send a message to start the conversation</p>
             )}
             {!loading && messages.length > 0 && (
                 messages.map((message, i) => (
-                    <div key={message._id} ref={lastMessageRef}>
+                    // <div key={message._id} ref={lastMessageRef}>
+                    <div key={message._id}>
                         <Message message={message} />
                     </div>
                 ))

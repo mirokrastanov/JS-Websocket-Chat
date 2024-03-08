52e05ff2f17d7931bc25845da33413433324ff2e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-08 18:49:58 +0200

    Render user fullName dynamically

--- client/src/components/messages/NoChatSelected.jsx
+++ client/src/components/messages/NoChatSelected.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from '../../context/AuthContext';
 
 const NoChatSelected = () => {
+    const { authUser } = useAuthContext();
+    console.log(authUser);
+
     return (
         <div className='flex items-center justify-center w-full h-full'>
             <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-                <p>Welcome 👋 John Doe ❄</p>
+                <p>Welcome 👋 {authUser.fullName} ❄</p>
                 <p>Select a chat to start messaging</p>
                 <TiMessages className='text-3xl md:text-6xl text-center' />
             </div>

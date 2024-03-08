8fc2ef1768ba452ca6ba71bb664a79b933e42e10
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-08 21:00:33 +0200

    Connect labels with unputs and add ids

--- client/src/pages/signup/GenderCheckbox.jsx
+++ client/src/pages/signup/GenderCheckbox.jsx
@@ -5,17 +5,19 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
     return (
         <div className='flex'>
             <div className='form-control'>
-                <label className={`label gap-2 cursor-pointer ${selectedGender === 'male' ? 'selected' : ''}`}>
+                <label htmlFor='male'
+                    className={`label gap-2 cursor-pointer ${selectedGender === 'male' ? 'selected' : ''}`}>
                     <span className='text-white'>Male</span>
-                    <input type='checkbox' className='checkbox border-gray-400'
+                    <input id='male' name='male' type='checkbox' className='checkbox border-gray-400'
                         checked={selectedGender === 'male'}
                         onChange={() => onCheckboxChange('male')} />
                 </label>
             </div>
             <div className='form-control'>
-                <label className={`label gap-2 cursor-pointer ${selectedGender === 'female' ? 'selected' : ''}`}>
+                <label htmlFor='female'
+                    className={`label gap-2 cursor-pointer ${selectedGender === 'female' ? 'selected' : ''}`}>
                     <span className='text-white'>Female</span>
-                    <input type='checkbox' className='checkbox border-gray-400'
+                    <input id='female' name='female' type='checkbox' className='checkbox border-gray-400'
                         checked={selectedGender === 'female'}
                         onChange={() => onCheckboxChange('female')} />
                 </label>

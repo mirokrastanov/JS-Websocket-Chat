7750e0020b42a7c846e995defad276ca1de86ba1
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-08 21:06:38 +0200

    Add input id and name

--- client/src/components/sidebar/SearchInput.jsx
+++ client/src/components/sidebar/SearchInput.jsx
@@ -27,7 +27,7 @@ const SearchInput = () => {
 
     return (
         <form className='flex items-center gap-2' onSubmit={handleSubmit}>
-            <input type='text' placeholder='Search…'
+            <input id='searchInput' name='searchInput' type='text' placeholder='Search…'
                 className='input input-bordered rounded-full'
                 value={search}
                 onChange={(e) => setSearch(e.target.value)} />

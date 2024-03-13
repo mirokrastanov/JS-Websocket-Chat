319bd713bbaa19e28934ac3a19040ed6a8b806a2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-03-13 11:18:48 +0200

    Deployment URL added

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,8 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io('http://localhost:5000', {
+            // const socket = io('http://localhost:5000', {  //==> dev
+            const socket = io('https://js-websocket-chat.onrender.com', {
                 query: {
                     userId: authUser._id,
                 },
